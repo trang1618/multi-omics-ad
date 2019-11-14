ff6df0c12bcb1cd39ecb3fae626238ac3cf22dfb
lelaboratoire <grixor@gmail.com>
2019-11-14 15:02:43 -0500

    change to continuous phenotype

--- mb-mdr/1-reformat-data.R
+++ mb-mdr/1-reformat-data.R
@@ -6,12 +6,25 @@ library(dplyr)
 library(data.table)
 
 data_dir <- here::here('data')
-filename <- list.files(data_dir, pattern = '*.txt')
+# filename <- list.files(data_dir, pattern = '*.txt')
+filenames <- list.files(data_dir, pattern = '*.csv')
 
-mydat <- paste(data_dir, filename, sep = '/') %>%
-  fread() %>%
-  dplyr::select(phenotype, everything()) %>%
-  replace(., is.na(.), -9) # recode NAs as -9 for MB-MDR
+for (filename in filenames) {
+  paste(data_dir, filename, sep = '/') %>%
+    fread() %>%
+    select(phenotype, everything()) %>%
+    select(-ID) %>%
+    replace(., is.na(.),-9) %>% # recode NAs as -9 for MB-MDR
+    fwrite(here::here(
+      'mb-mdr', 
+      'reformatted-data', 
+      gsub('.csv', '.txt', filename)), sep = '\t') 
+}
 
-mydat %>%
-  fwrite(here::here('mb-mdr', 'reformatted-data', filename), sep = '\t')
+
+# outcomes <- paste(data_dir, fileout, sep = '/') %>%
+#   fread() %>%
+#   dplyr::select(phenotype, everything())
+
+# mydat %>%
+#   fwrite(here::here('mb-mdr', 'reformatted-data', filename), sep = '\t')
